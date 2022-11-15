9a9f56088f12f6da62878c327d9a5970b4ba6287
lbrandstaetter <luca.brandstaetter06@htl-salzburg.ac.at>
2022-11-15 23:04:05 +0100

    commited changes

--- 01lbrandstaetter-fingeruebungEindimensionalesArray/Assets/MyGame/Scripts/arrayFingeruebung.cs
+++ 01lbrandstaetter-fingeruebungEindimensionalesArray/Assets/MyGame/Scripts/arrayFingeruebung.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class arrayFingeruebung : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int[] array1 = new int[4];
+    int[] array2 = new int[4];
+    int[] array3 = new int[7] {31,32,33,34,35,36,37};
+    string[] array4 = new string[10] 
+    {"array4","array4","array4","array4","array4",
+     "array4","array4","array4","array4","array4"};
+    
+    GameObject[] arrayColoredImagesRed;
+    
+    
     void Start()
     {
+        arrayColoredImagesRed = GameObject.FindGameObjectsWithTag("RedImage");
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        Debug.Log(arrayColoredImagesRed[0].name);
+        Debug.Log(array1[0]);
+        Debug.Log(array4[0]);
         
+        Debug.Log(arrayColoredImagesRed[arrayColoredImagesRed.Length - 1].name);
+        Debug.Log(array1[array1.Length - 1]);
+        Debug.Log(array4[array4.Length - 1]);
+        
+        for (int i = 0; i < array4.Length; i++) 
+        {
+            Debug.Log("Der Wert der Elemente [" + i + "] hat den Wert:" + array4[i];
+        }
     }
 }
