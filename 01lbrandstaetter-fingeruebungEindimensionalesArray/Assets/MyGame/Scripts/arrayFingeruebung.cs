9c9b8baa6d8ad6a0e3cc661032de4dc5ce4ecb64
lbrandstaetter <luca.brandstaetter06@htl-salzburg.ac.at>
2022-11-22 16:21:19 +0100

    Script final commit

--- 01lbrandstaetter-fingeruebungEindimensionalesArray/Assets/MyGame/Scripts/arrayFingeruebung.cs
+++ 01lbrandstaetter-fingeruebungEindimensionalesArray/Assets/MyGame/Scripts/arrayFingeruebung.cs
@@ -25,8 +25,6 @@ public class arrayFingeruebung : MonoBehaviour
         Debug.Log(array1[array1.Length - 1]);
         Debug.Log(array4[array4.Length - 1]);
 
-        Werte();
-
         for (int i = 0; i < array3.Length; i++)
         {
             Debug.Log(array3[i]);
@@ -50,9 +48,89 @@ public class arrayFingeruebung : MonoBehaviour
         Debug.Log(array4[2]);
 
         arrayColoredImagesRed[3].GetComponent<SpriteRenderer>().color = Color.green;
+
+    int[] array5 = new int[51];
+        for (int i = 0; i<array5.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                array5[i] = i + 20;
+            }
+
+        }
+
+        int[] array6 = new int[501];
+    for (int i = 0; i < array6.Length; i++)
+    {
+    if (i % 2 == 0)
+    {
+        array6[i] = i;
     }
-        void Werte()
-   {
-        Debug.Log(array1[0] + " " + array1[1] + " " + array1[2] + " " + array1[3]);
-   }
+
+    }
+
+    int[] array7 = new int[501];
+    for (int i = 0; i < array7.Length; i++)
+    {
+    if (i % 2 == 1)
+         {
+        array7[i] = i;
+         }
+
+    }
+
+    string[] array8 = new string[101];
+    for (int i = 0; i < array8.Length; i++)
+    {
+    if (i % 2 == 0)
+    {
+        array8[i] = "a";
+        }  
+
+    else if (i % 2 == 1)
+        {
+        array8[i] = "b";
+        }
+    }
+
+       int[] array9 = new int[1001];
+    for (int i = 0; i < array9.Length; i++)
+        {
+            array9[i] = Random.Range(0, 1000);
+            Debug.Log(array9[i]);
+        }
+
+    System.Array.Sort(array9);
+
+    for (int i = 0; i < array9.Length; i++)
+        {
+            Debug.Log(array9[i]);
+        }
+
+
+
+    int[] array11 = new int[1001];
+    for (int i = 0; i < array11.Length; i++)
+        {
+            array11[i] = Random.Range(0, 1000);
+            Debug.Log(array9[i]);
+        }
+
+        System.Array.Sort(array11);
+
+            Debug.Log(array11[0] + " und " + array11[array11.Length - 1]);
+
+
+
+    int[] array12 = new int[1001];
+    for (int i = 0; i < array12.Length; i++)
+        {
+            array12[i] = Random.Range(0, 1000);
+        }
+
+    int randomNumber = Random.Range(0, 1000);
+
+        Debug.Log(System.Array.IndexOf(array12, randomNumber));
+
+    }
 }
